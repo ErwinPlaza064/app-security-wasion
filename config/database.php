cd60b41408da22f9916700f7dbe038d8cc9b4ef8
MartinezErwin <612110504@utsalamanca.edu.mx>
2026-02-09 09:43:35 -0600

    feat: Add initial database configuration file with support for multiple database drivers and Redis.

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST') ?: env('MYSQLHOST') ?: '127.0.0.1',
-            'port' => env('DB_PORT') ?: env('MYSQLPORT') ?: '3306',
+            'host' => env('DB_HOST') ?: env('MYSQLHOST') ?: env('MYSQL_HOST') ?: '127.0.0.1',
+            'port' => env('DB_PORT') ?: env('MYSQLPORT') ?: env('MYSQL_PORT') ?: '3306',
             'database' => env('DB_DATABASE') ?: env('MYSQLDATABASE') ?: env('MYSQL_DATABASE') ?: 'laravel',
-            'username' => env('DB_USERNAME') ?: env('MYSQLUSER') ?: 'root',
-            'password' => env('DB_PASSWORD') ?: env('MYSQLPASSWORD') ?: '',
+            'username' => env('DB_USERNAME') ?: env('MYSQLUSER') ?: env('MYSQL_USER') ?: 'root',
+            'password' => env('DB_PASSWORD') ?: env('MYSQLPASSWORD') ?: env('MYSQL_PASSWORD') ?: '',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -59,6 +59,8 @@ return [
                 PDO::ATTR_EMULATE_PREPARES => true,
                 PDO::MYSQL_ATTR_LOCAL_INFILE => true,
                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
+                // Forzamos a que no use SSL si falla el handshake interno de Railway
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
 
